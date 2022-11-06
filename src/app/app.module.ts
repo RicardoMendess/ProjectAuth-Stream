bd5534f1d83ff21339cc310d1e8e4917bc92e835
RicardoMendess <ricardomendesprofissional@gmail.com>
2022-11-06 11:41:44 -0300

    fix: adição de novos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginAreaComponent } from './pages/login-area/login-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginAreaComponent
   ],
   imports: [
     BrowserModule,
